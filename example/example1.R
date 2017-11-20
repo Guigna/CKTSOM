8c2c0c90b396eee207572857d1c0442bda38f1c0
Javier Aros Mendoza <jaros11@alumnos.utalca.cl>
2017-11-20 12:23:17 -0300

    example change

--- example/example1.R
+++ example/example1.R
@@ -22,7 +22,6 @@ ti <- proc.time() # start timer
 neurons <- train(numberOfChildrenperNode,treeHeight,initialLearningRate,finalLearningRate,initialRadius,finalRadius,numberOfIterations, data)
 tf <-proc.time()    # stop timer
 tf-ti #print execution time
-beep("mario")
 ##visualization phase
 ##Display phase without grouping
 clusterVisualization(data,neurons,numberOfChildrenperNode) #plot the scatter plot
